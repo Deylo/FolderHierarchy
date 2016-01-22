e7538e289f380379ba9ac846eca2eb06ccd45728
Deylo <Deylo@users.noreply.github.com>
2016-01-22 20:57:47 +0200

    v 0.1

--- js.js
+++ js.js
@@ -1,21 +1,64 @@
 /**
  * Created by dmitriy on 21.01.16.
  */
-
-
-function func(context){
-        var fElChil = context.parentElement.children[1];
-        if (fElChil.hidden) {
-            fElChil.hidden = false;
-        }
-        else fElChil.hidden = true;
+var path = {
+    1 : '1.1/1.1.1/1.1.1.1/',
+    2 : '2.1/2.1.1/2.1.1.1./2.1.1.1.1/',
+    3 : '3.1/3.1.1/'
 }
 
-function add(context){
-        var text = prompt('Please write new fold');
-        if (text) {
+function createList(){
+    for(var i in path){
+        addToPage(path[i].split('/').slice(0,-1),i);
+    }
+}
+
+function addToPage(arr,file){
+    var i = 0;
+    var isRoot=true;
+    alert(i);
+    var context=document.body;
+    if(document.body.children.length===1) {
+        var ul = document.createElement('ul');
+        ul.innerHTML = '<li id="a" type="none" onclick="add(this)">Add</li>';
+        context.appendChild(ul);
+    }
+
+    (function f(){
+        var n =0;
+        if(!i) {
+            context = document.getElementById('a');
+
+        }
+        else{
+            n = context.parentElement.getElementsByClassName('add').length;
+            isRoot?context=context.parentElement.getElementsByClassName('add')[n-1]:context=context.parentElement.getElementsByClassName('add')[0];
+            isRoot=false;
+        }
+        if(i===arr.length)
+            add(context, file, true);
+        else{
+            add(context,arr[i++]);
+            f();
+        }
+    })();
+}
+
+function func(context){
+        var ElChil = context.parentElement.children[1];
+        if (ElChil.hidden) {
+            ElChil.hidden = false;
+        }
+        else ElChil.hidden = true;
+}
+
+function add(context,text,isFille){
+        var buf='';
+        text?this.text=text:this.text=prompt('Please write new fold');
+        isFille?buf=this.text:buf='<span onclick="func(this)">+</span>'+this.text+'<ul hidden="true"><li class="add"  type="none" onclick="add(this)">Add</li></ul>';
+    if (this.text) {
             var li = document.createElement('li');
-            li.innerHTML = '<span onclick="func(this)">+</span>'+text+'<ul hidden="true"><li  type="none" onclick="add(this)">Add</li></ul>';
+            li.innerHTML =buf;
             var n = context.parentElement.children.length;
             context.parentElement.insertBefore(li, context.parentElement.children[n - 1]);
     }
